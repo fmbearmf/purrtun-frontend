6da79da91d6ec4f34558bcf09a4dfc0ae5fa1d5e
sus-amongus <xdanelite@gmail.com>
2024-08-24 16:03:46 -0400

    Removed color transition from body; reimplemented it in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-dvh flex flex-col transition-colors duration-500">
+  <div :class="['min-h-dvh flex flex-col transition-colors duration-500', colorModeClass]">
     <Header />
     <div class="flex-grow flex flex-col sm:flex-row my-5">
       <div class="flex-grow sm:w-1/3 hidden md:block">
@@ -12,3 +12,12 @@
     </div>
   </div>
 </template>
+
+<script setup>
+import { computed } from 'vue';
+const colorMode = useColorMode();
+
+const colorModeClass = computed(() => {
+  return colorMode.preference === 'dark' ? 'bg-neutral-800' : 'bg-neutral-100';
+});
+</script>
